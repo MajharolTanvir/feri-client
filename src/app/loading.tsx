407259fb9db6babd8abf75c937f876228f7afa35
MajharolTanvir <majharul5456@gmail.com>
2023-11-30 13:16:30 +0600

    Error, loading, not found page added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,17 @@
+'use client'
 import React from 'react'
+import { Triangle } from 'react-loader-spinner';
 
 const Loading = () => {
   return (
-    <div>Loading....</div>
-  )
+    <Triangle
+      height="80"
+      width="80"
+      color="bg-slate-900"
+      ariaLabel="triangle-loading"
+      visible={true}
+    />
+  );
 }
 
 export default Loading
